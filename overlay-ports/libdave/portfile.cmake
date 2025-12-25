e8583ac7b5bf67f4c8a8224fe13efcd325202a7a
jontitorr <mail@jontitor.com>
2025-12-25 09:27:57 -0600

    build: :wrench: add disable-logging feature to libdave port; prefer no logging by default

--- overlay-ports/libdave/portfile.cmake
+++ overlay-ports/libdave/portfile.cmake
@@ -1,9 +1,19 @@
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        disable-logging DISABLE_LOGGING
+)
+
+set(PATCHES "vcpkg-support.patch")
+if(DISABLE_LOGGING)
+    list(APPEND PATCHES "disable-logging.patch")
+endif()
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO discord/libdave
     REF "${VERSION}"
     SHA512 6284ba5842519eb3be64bd2dd98dacdd75443738ca1b5b5a0642d2b21576cc57b28b0e0cf517654efd2b22af7160d818f951404132069f5907c5dc528f735165
-    PATCHES "vcpkg-support.patch"
+    PATCHES ${PATCHES}
 )
 
 vcpkg_cmake_configure(
@@ -13,7 +23,6 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME libdave CONFIG_PATH lib/cmake/libdave)
 
-# Install license
 vcpkg_install_copyright(
     FILE_LIST "${SOURCE_PATH}/LICENSE"
 )
